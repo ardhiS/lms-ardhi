849a2f8fed43dc46be68509a36a5c5bf40eb1895
ardhisasongko <ardhisasongko69@guru.smp.belajar.id>
2026-02-16 19:10:27 +0700

    try change route for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { DarkModeProvider } from './context/DarkModeContext';
 import { lazy, Suspense } from 'react';
